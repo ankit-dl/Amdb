85d75d5f9e026a22c5ae86449e417ccd9a5c0331
ankit thakur <ankit.dl@live.com>
2020-09-15 13:58:47 +0530

    removed test double

--- app/src/test/java/com/altimetric/amdb/model/RepositoryTest.java
+++ app/src/test/java/com/altimetric/amdb/model/RepositoryTest.java
@@ -3,6 +3,7 @@ package com.altimetric.amdb.model;
 import androidx.arch.core.executor.DefaultTaskExecutor;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.altimetric.amdb.model.remote.ApiService;
 import com.altimetric.amdb.model.remote.BaseResult;
@@ -12,17 +13,19 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
 
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.OngoingStubbing;
 
 import java.io.IOException;
 
-import okhttp3.Call;
 import okhttp3.Request;
 import okio.Timeout;
+import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
@@ -36,65 +39,27 @@ public class RepositoryTest {
     Repository SUT;
     @Mock
     ApiService service;
+    @Mock
+    Call<BaseResult> resp;
     @Rule
     public InstantTaskExecutorRule executor = new InstantTaskExecutorRule();
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp()  {
         SUT = new Repository(service);
     }
 
     @Test
     public void test_search() {
-        Mockito.when(service.getSearchResult("test")).thenReturn(new CallTestDouble<>());
-        // Mockito.when().thenReturn(null);
-       // SUT.getLoadingState();
-     //   SUT.getLoadingState();
+        Mockito.when(service.getSearchResult("test")).thenReturn(resp);
+        SUT.getSearchResult("test");
+        Mockito.verify(service, Mockito.times(1)).getSearchResult("test");
+
 
         assertNotNull(SUT.getSearchResult("test"));
-        // assertNull(SUT.getSearchLiveDate());
-    }
-}
+        assertNotNull(SUT.getLoadingState());
+        assertNotNull(SUT.getLoadingState());
 
-class CallTestDouble<T> implements retrofit2.Call<T> {
-
-    @Override
-    public Response<T> execute() throws IOException {
-        return null;
-    }
-
-    @Override
-    public void enqueue(Callback<T> callback) {
-
-    }
-
-    @Override
-    public boolean isExecuted() {
-        return false;
-    }
-
-    @Override
-    public void cancel() {
-
-    }
-
-    @Override
-    public boolean isCanceled() {
-        return false;
-    }
-
-    @Override
-    public retrofit2.Call<T> clone() {
-        return null;
-    }
-
-    @Override
-    public Request request() {
-        return null;
-    }
-
-    @Override
-    public Timeout timeout() {
-        return null;
+        // assertNull(SUT.getSearchLiveDate());*/
     }
 }
